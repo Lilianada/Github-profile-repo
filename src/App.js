83403d0412d0a6a3ac42a4a3fd34db0bd3bf5c58
Lilianada <lilibest30.lb@gmail.com>
2022-11-01 17:41:20 -0400

    Error page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ErrorPage from './components/404Page/404Page';
 import Login from './components/Authentication/Login';
 import RepositoryPage from './components/ProfilePage/RepoPage';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <RepositoryPage/>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/repositories' element={ <RepositoryPage/>} />
+        <Route path='*' element={<ErrorPage/>} />
+      </Routes>
   </div>
   );
 }
--- src/components/ProfilePage/RepoPage.jsx
+++ src/components/ProfilePage/RepoPage.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
-import styled from "styled-components"
-import Header from "../Header/NavBar/NavBar"
+import styled from "styled-components";
+import Header from "../Header/NavBar/NavBar";
+import axios from "axios";
 
 const Wrapper = styled.main`
 
@@ -28,33 +29,52 @@ const Right = styled.area`
 `
 
 export default function RepositoryPage () {
-    const url = `https://api.github.com/users/lilianada/repos`;
     //     Link: <https://api.github.com/user/repos?page=3&per_page=100>; rel="next",
     //   <https://api.github.com/user/repos?page=50&per_page=100>; rel="last"
     // Octokit.js
     // https://github.com/octokit/core.js#readme
-
-    // const octokit = new Octokit({
-    //     auth: 'YOUR-TOKEN'
-    // })
     
-    // await octokit.request('GET /users/lilianada/repos', {
-    //     username: 'LILIANADA'
-    // })
+    // const octokit = new Octokit({
+        //     auth: 'YOUR-TOKEN'
+        // })
+        
+        // await octokit.request('GET /users/lilianada/repos', {
+            //     username: 'LILIANADA'
+            // })
+            
+    const [data, setData] = useState([]);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+
+    const fetchData = () => {
+        const url = `https://api.github.com/users/lilianada/repos`;
+        console.log(url)
+        axios.get(url).then((response) => {
+        setData(response.data.results);
+        console.log(response.data.results)
+        setLoading(true);
+        }).catch((err) => {
+        setError(err);
+        setLoading(true);
+        }).finally(() => {
+        setLoading(false);
+        })
+    }
 
     useEffect (() => {
-        axios.get(url).then((response) => {
-          setData(response.data.results);
-          console.log(response.data.results)
-          setLoading(true);
-        }).catch((err) => {
-          setError(err);
-          setLoading(true);
-        }).finally(() => {
-            setLoading(false);
-        })
-      }, [url]);
-       return { data, error, loading };  m
+        fetchData()
+    }, [])
+
+    const nextPage = () => {
+        setPage(page => page + 1);
+        fetchData();
+    }
+
+    const prevPage = () => {
+        setPage(page => page - 1);
+        fetchData();
+    }
     
     return(
         <Wrapper>
@@ -63,14 +83,20 @@ export default function RepositoryPage () {
             <Head>
                 <p>Repositories</p>
             </Head>
-            <Body>
-                <Left>
+            {
+                loading ? (<p>loading...</p>) : (
+                    data.map((item) => {
+                <Body>
+                    <Left>
+                            
+                    </Left>
+                    <Right>
 
-                </Left>
-                <Right>
+                    </Right>
+                </Body>
+}) )
 
-                </Right>
-            </Body>
+            }
         </Wrapper>
     )
 }
