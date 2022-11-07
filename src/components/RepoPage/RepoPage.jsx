79fea48bd73e67192817d0c069b5db07279b0649
Lilianada <lilibest30.lb@gmail.com>
2022-11-07 08:23:28 -0500

    ...

--- src/components/RepoPage/RepoPage.jsx
+++ src/components/RepoPage/RepoPage.jsx
@@ -4,31 +4,15 @@ import './RepoPage.css';
 
 export default function RepositoryPage() {
   
-  const { attorneys } = useAttorneyContext();
-  const getRepo = (id) => {
-    const filtered = repos.filter(
-      (repo) => parseInt(repo.id) === parseInt(id)
-    );
-    return filtered[0];
-  };
+  // const { attorneys } = useAttorneyContext();
+  // const getRepo = (id) => {
+  //   const filtered = repos.filter(
+  //     (repo) => parseInt(repo.id) === parseInt(id)
+  //   );
+  //   return filtered[0];
+  // };
   // return { getRepo };
-  const fetchData = () => {
-    const url = `https://api.github.com/users/lilianada/repos?page=${page}&per_page=12`;
-    setLoading(true);
-    axios
-      .get(url)
-      .then((response) => {
-        setData(response.data);
-        console.log(response.data);
-    })
-    .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-          setLoading(false);
-      });
-  };
-
+  
   return (
     <main className="wrapper">
       <Header/>
